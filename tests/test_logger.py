863b9968b84dc175c5347711786d215412edf624
TheLordVier <messi060591@gmail.com>
2023-09-18 10:57:13 +0300

    updated tests in the project

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -5,26 +5,36 @@ from logger import setup_logging, log_info, log_error
 class TestLogging(unittest.TestCase):
 
     def test_log_info(self):
+        """
+        Set up the log file path and configure logging
+        """
         log_file_path = 'logs/test.log'
         setup_logging(log_file_path)
 
+        # Log an information message
         message = 'This is an info message.'
         log_info(message)
 
+        # Check if the message is present in the log file
         with open(log_file_path, 'r') as log_file:
             logs = log_file.read()
-            self.assertTrue(message in logs)
+            self.assertTrue(message in logs)  # Check if the message is in the logs
 
     def test_log_error(self):
+        """
+        Set up the log file path and configure logging
+        """
         log_file_path = 'logs/test.log'
         setup_logging(log_file_path)
 
+        # Log an error message
         message = 'This is an error message.'
         log_error(message)
 
+        # Check if the error message is present in the log file
         with open(log_file_path, 'r') as log_file:
             logs = log_file.read()
-            self.assertTrue(message in logs)
+            self.assertTrue(message in logs)  # Check if the error message is in the logs
 
 
 if __name__ == '__main__':
--- tests/test_mail_utils.py
+++ tests/test_mail_utils.py
@@ -5,36 +5,41 @@ from mail_utils import send_email
 
 class TestSendEmail(unittest.TestCase):
 
-    @patch('mail_utils.smtplib.SMTP')
+    @patch('mail_utils.smtplib.SMTP')  # Use the patch decorator to mock the SMTP class from mail_utils.
     def test_send_email_success(self, mock_smtp):
-        # Set up mocks
+        """
+        Set up mocks for a successful email send
+        """
         mock_smtp_instance = mock_smtp.return_value
         mock_smtp_instance.starttls.return_value = None
         mock_smtp_instance.login.return_value = None
         mock_smtp_instance.sendmail.return_value = None
         mock_smtp_instance.quit.return_value = None
 
-        # Call the function
+        # Call the send_email function with mock SMTP server and capture the result
         success, message = send_email('smtp.example.com', 587, 'username', 'password',
                                       'to@example.com', 'Test Subject', 'Test Message')
 
-        # Assertions
+        # Assertions to test if the email sending was successful
         self.assertTrue(success)
         self.assertEqual(message, 'Email sent to to@example.com with subject: Test Subject')
 
-    @patch('mail_utils.smtplib.SMTP')
+    @patch('mail_utils.smtplib.SMTP')  # Mock the SMTP class again for a failure scenario
     def test_send_email_failure(self, mock_smtp):
-        # Set up mocks to raise an exception
+        """
+        Set up mocks to simulate an exception during email send
+        """
         mock_smtp_instance = mock_smtp.return_value
         mock_smtp_instance.starttls.return_value = None
         mock_smtp_instance.login.return_value = None
         mock_smtp_instance.sendmail.side_effect = Exception('Mocked exception')
 
-        # Call the function
-        success, message = send_email('smtp.example.com', 587, 'username', 'password',
-                                      'to@example.com', 'Test Subject', 'Test Message')
+        # Call the send_email function with mock SMTP server and capture the result
+        success, message = send_email('smtp.example.com', 587, 'username',
+                                      'password', 'to@example.com', 'Test Subject',
+                                      'Test Message')
 
-        # Assertions
+        # Assertions to test if the email sending failed
         self.assertFalse(success)
         self.assertIn('Failed to send email to to@example.com.', message)
 
